6695d02fd2bccdf3b189780b4d19fc39c751c839
Osku Sirpoma <osku.sirpoma@gmail.com>
2018-01-17 22:48:35 +0200

    :_

---
+++ GameProject/src/data/Boot.java
@@ -0,0 +1,56 @@
+package data;
+
+import static helpers.Artist.BeginSession;
+import static helpers.Artist.QuickLoad;
+
+import org.lwjgl.opengl.Display;
+
+import helpers.Clock;
+
+public class Boot {
+	
+	public Boot() {
+		
+		BeginSession();
+		
+		int[][] map = {
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 2, 1, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 1, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	
+		};
+		
+		TileGrid grid = new TileGrid(map);
+		grid.setTile(6, 4, grid.getTile(5,4).getType());
+		Enemy e = new Enemy(QuickLoad("UFO64"), grid.getTile(10, 10), 64, 64, 6);
+		Wave wave = new Wave(20, e);
+		Player player = new Player(grid);
+		while(!Display.isCloseRequested()) {
+			Clock.update();
+			grid.Draw();
+			wave.Update();
+			player.Update();
+			
+			
+			Display.update();
+			Display.sync(60);
+		}
+		
+		Display.destroy();
+	}
+	
+	public static void main(String [] args) {
+		new Boot();
+	}
+}
